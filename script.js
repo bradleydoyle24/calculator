1b04524619b0af5f3486ce43809e648bafa92e90
Bradley <bradleydoyle24@gmail.com>
2023-07-21 22:08:46 -0400

    Add pseudocode in script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+const for each button (id)
+  event listener
+
+const for display
+  able to alter text content
+
+variable to store user clicked option
+  add on until operator is clicked
+
+variable to store next user clicked option
+  create on the fly as user clicks
+
+variable to store user clicked operator
+
+variable to show current result
+  so user can use to calculate with future operands
+*/
