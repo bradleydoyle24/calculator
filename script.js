186e111c59a0caba59cb00aa06e4d480673b06d2
Bradley <bradleydoyle24@gmail.com>
2023-08-03 20:56:39 -0400

    Previous commit performed improperly

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-/*
-const for each button (id)
-  event listener
-
-const for display
-  able to alter text content
-
-variable to store user clicked option
-  add on until operator is clicked
-
-variable to store next user clicked option
-  create on the fly as user clicks
-
-variable to store user clicked operator
-
-variable to show current result
-  so user can use to calculate with future operands
-*/
-
-
-
 // Display should be an array that is turned into a number?
 const display = document.querySelector('#display');
 
